413bb4421d09af0adbdeb02343d0d751426bb951
Alexey Brovko <alexey.brovko@jag.se>
2021-05-25 19:00:28 +0500

    Add Block model

---
+++ Web/Models/Models.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Web.Models
+{
+    public class LinkModel
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class BlockModel
+    {
+        public string Title { get; set; }
+        public IList<LinkModel> Links { get; set; }
+    }
+}
